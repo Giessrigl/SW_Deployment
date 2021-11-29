f48e05d5dd43d8deb93ea6ea954755d51e8a74a9
Giessrigl Christian <christian.giessrigl@gmail.com>
2021-11-29 17:10:23 +0100

    finish Lab4

--- Lab4/deploy.bat
+++ Lab4/deploy.bat
@@ -1,5 +1,4 @@
 kubectl apply -f .\namespace.yaml
-kubectl apply -f .\secret.yaml
 kubectl apply -f .\storageclass.yaml -n wordpress-ns
 kubectl apply -f .\persistentvolumeclaims.yaml -n wordpress-ns
 kubectl apply -f .\services.yaml -n wordpress-ns
